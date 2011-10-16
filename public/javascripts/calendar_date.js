061a36ec1c874d8962981d7892c8941cfc28b13c
amseledka <amseledka@gmail.com>
2011-10-16 11:27:33 +0300

    calendar views

--- public/javascripts/calendar_date.js
+++ public/javascripts/calendar_date.js
@@ -1,8 +1,8 @@
 jQuery(function($) {
-  $("form.new_calendar_record").ajaxForm();
+  $("form.new_admin_calendar_record").ajaxForm();
   $("table.calendar td").click(function() {
    $(this).toggleClass("free");
    $(this).toggleClass("busy");
-   $(this).find("form.new_calendar_record:first").submit();
+   $(this).find("form.new_admin_calendar_record:first").submit();
   });
 });

a9e582db4e5ceb394553fe1cef6951e961fb6b9c
amseledka <amseledka@gmail.com>
2011-11-04 15:30:43 +0200

    static pages new index

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Ianphoto::Application.routes.draw do
   match '/signup/:invite_code' => 'users#new', :as => 'redeem_invitation'
   match '/publications' => 'static_pages#index', :as => :publications
   match '/publications/:id' => 'static_pages#show'
+  match '/:slug.html' => 'static_pages#show', :as => :static_page
   
   resources :users do
     resources :categories do
@@ -47,7 +48,6 @@ Ianphoto::Application.routes.draw do
       end
     end
     resources :invites
-    resources :static_pages
     root :to => 'main#index'
   end
   resources :calendar_records

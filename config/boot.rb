d40d3ba86dbbaab8bf28a3390d7f68edb24ba83b
mike <tenore.hatemail@gmail.com>
2011-11-06 13:23:01 +0200

    added ruby version check

--- config/boot.rb
+++ config/boot.rb
@@ -5,4 +5,6 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 require 'yaml'
-YAML::ENGINE.yamler= 'syck'
+if RUBY_VERSION != "1.8.7"
+  YAML::ENGINE.yamler= 'syck'
+end

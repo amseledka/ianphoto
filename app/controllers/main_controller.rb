1a64ea582988c5b1278de7d3056e8ffd5802c412
mike <tenore.hatemail@gmail.com>
2011-11-22 23:29:14 +0200

    better main page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
   def index
-    @photos = Photo.shuffled.take(12)
+    @photos = Photo.shuffled.take(20)
     @big_photo = @photos.shift
     @static_pages = StaticPage.latest.take(3)
   end
